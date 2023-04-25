39eb5533947ae3644be92f361a1bdb2c1771f201
jovi <jovisimons009@gmail.com>
2023-04-25 20:34:36 +0200

    routing via env

--- src/main/java/jovisimons/dekeet/APIGateway/config/FirebaseConfig.java
+++ src/main/java/jovisimons/dekeet/APIGateway/config/FirebaseConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig{
--- src/main/java/jovisimons/dekeet/APIGateway/config/GatewayConfig.java
+++ src/main/java/jovisimons/dekeet/APIGateway/config/GatewayConfig.java
@@ -1,6 +1,7 @@
 package jovisimons.dekeet.APIGateway.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,16 +13,21 @@ public class GatewayConfig {
     @Autowired
     AuthenticationFilter filter;
 
+    @Value("${destinations.authurl}")
+    String authUrl;
+
+    @Value("${destinations.userurl}")
+    String userUrl;
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("user-service", r -> r.path("/user/**")
                         .filters(f -> f.filter(filter))
-                        .uri("http://localhost:8094"))
+                        .uri(userUrl))
 
                 .route("auth-service", r -> r.path("/auth/**")
                         .filters(f -> f.filter(filter))
-                        .uri("lb://auth-service"))
+                        .uri(authUrl))
                 .build();
     }
 
