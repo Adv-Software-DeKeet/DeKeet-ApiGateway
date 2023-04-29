67b356a39caa20fc663db1876dfb2bfe69c02350
jovi <jovisimons009@gmail.com>
2023-04-29 17:05:35 +0200

    gateway CORS

--- src/main/java/jovisimons/dekeet/APIGateway/config/GatewayConfig.java
+++ src/main/java/jovisimons/dekeet/APIGateway/config/GatewayConfig.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class GatewayConfig {
@@ -21,10 +22,22 @@ public class GatewayConfig {
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("user-service", r -> r.path("/api/user/**")
+
+                .route(r -> r.path("/api/user/**")
+                        .and().method(HttpMethod.GET)
                         .filters(f -> f.filter(filter))
                         .uri(userUrl))
-
+                .route(r -> r.path("/api/user")
+                        .and().method(HttpMethod.POST)
+                        .uri(userUrl))
+                .route(r -> r.path("/api/user/**")
+                        .and().method(HttpMethod.DELETE)
+                        .filters(f -> f.filter(filter))
+                        .uri(userUrl))
+                .route(r -> r.path("/api/user/**")
+                        .and().method(HttpMethod.PUT)
+                        .filters(f -> f.filter(filter))
+                        .uri(userUrl))
                 .route("auth-service", r -> r.path("/auth/**")
                         .filters(f -> f.filter(filter))
                         .uri(authUrl))
