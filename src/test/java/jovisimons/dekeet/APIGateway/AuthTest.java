c8f63de4fff6c1e14eb0a4a3300c10047b8d1c51
jovi <jovisimons009@gmail.com>
2023-05-10 15:09:01 +0200

    fixed test not running

--- src/test/java/jovisimons/dekeet/APIGateway/AuthTest.java
+++ src/test/java/jovisimons/dekeet/APIGateway/AuthTest.java
@@ -1,16 +1,13 @@
 package jovisimons.dekeet.APIGateway;
 
 import com.google.firebase.auth.FirebaseAuthException;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import com.google.firebase.auth.FirebaseAuth;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class AuthTest {
 
